acac50c8b4a95ff1f20eecd868a2d3a6901671b7
Nathan Gaul <nathanjgaul@gmail.com>
2020-07-22 15:41:39 -0600

    Enemy Stats + Exports + Setget

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,9 +1,9 @@
 extends KinematicBody2D
 
-const ACCELERATION := 500
-const MAX_SPEED := 80
-const ROLL_SPEED := 125
-const FRICTION := 500
+export(int) var ACCELERATION := 500
+export(int) var MAX_SPEED := 80
+export(int) var ROLL_SPEED := 125
+export(int) var FRICTION := 500
 
 enum {
 	MOVE,
