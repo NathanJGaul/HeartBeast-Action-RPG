e7e8b8948ad3a704b9813259bcc6fc29f3fe2e8b
Nathan Gaul <nathanjgaul@gmail.com>
2020-07-23 07:16:28 -0600

    Video 15 Enemy Death Effect

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+const EnemyDeathEffect = preload("res://Effects/EnemyDeathEffect.tscn")
+
 var knockback = Vector2.ZERO
 
 onready var stats = $Stats
@@ -16,6 +18,8 @@ func _on_Hurtbox_area_entered(area):
 	stats.health -= area.damage
 	knockback = area.knockback_vector * 150
 
-
 func _on_Stats_no_health():
 	queue_free()
+	var enemyDeathEffect = EnemyDeathEffect.instance()
+	get_parent().add_child(enemyDeathEffect)
+	enemyDeathEffect.global_position = global_position
