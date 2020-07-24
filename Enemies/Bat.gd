aba6dbf621935f2be6ba5374730da9cf7a3a90b9
Nathan Gaul <nathanjgaul@gmail.com>
2020-07-24 13:20:23 -0600

    Video 17 Player Stats + Enemy Attack

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -21,6 +21,7 @@ var state = CHASE
 onready var stats = $Stats
 onready var playerDetectionZone = $PlayerDetectionZone
 onready var sprite = $AnimatedSprite
+onready var hurtbox = $Hurtbox
 
 func _ready():
 	print(stats.max_health)
@@ -56,6 +57,8 @@ func seek_player():
 func _on_Hurtbox_area_entered(area):
 	stats.health -= area.damage
 	knockback = area.knockback_vector * 150
+	hurtbox.start_invincibility(0.5)
+	hurtbox.create_hit_effect()
 
 func _on_Stats_no_health():
 	queue_free()
